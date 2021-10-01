0bddb0ea1d7093e70eb6c18c1b09b0874eca153d
MiguelBarreraDev <barreram987@gmail.com>
2021-10-01 00:33:08 -0500

    Update 101 task 0v01

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
   */
 int main(void)
 {
-	int sizeStr=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n", sizeStr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
