2b88a61a19fc0468e8f2abd6ef6606515210a4a4
MiguelBarreraDev <barreram987@gmail.com>
2021-10-01 00:27:51 -0500

    update task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
   * main - Entry point
+  *
+  * Return: Always 1(succes)
   */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	int sizeStr=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n", sizeStr);
 	return (1);
 }
