349ded8029c3a872c574229e1f1f05a14a8602f7
MiguelBarreraDev <82726832+MiguelBarreraDev@users.noreply.github.com>
2021-09-30 19:03:16 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-struct Types
-{
-        char text;
-        int numInt;
-        long int numLong;
-        long long int numLongLong;
-        float numFloat;
-}ctnType;
 int main(void)
 {
-        struct Types * ptrStruct = &ctnType;
-        printf("Size of a char: %lu byte(s)\n", sizeof(ptrStruct->text));
-        printf("Size of a int: %lu byte(s)\n", sizeof(ptrStruct->numInt));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(ptrStruct->numLong));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(ptrStruct->numLongLong));
-        printf("Size of a float: %lu byte(s)\n", sizeof(ptrStruct->numFloat));
+        printf("Size of a char: %lu byte(s)\n", sizeof(char));
+        printf("Size of an int: %lu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));
         return 0;
 }
