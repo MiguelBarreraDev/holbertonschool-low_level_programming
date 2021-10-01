2b7997da5bc07e5d65631c6a35ce8ebadee26795
MiguelBarreraDev <82726832+MiguelBarreraDev@users.noreply.github.com>
2021-09-30 19:19:42 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
         printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
         printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
         printf("Size of a float: %lu byte(s)\n", sizeof(float));
-        fprintf(stderr, "[Anything]");
+        fprintf(stderr, "[Anything]\n");
         return (0);
 }
