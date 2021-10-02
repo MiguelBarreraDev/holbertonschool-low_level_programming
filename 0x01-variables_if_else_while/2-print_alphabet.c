85a107a5f5525f543917a92558b980198651dce4
MiguelBarreraDev <barreram987@gmail.com>
2021-10-02 03:47:41 -0500

    Update task2 - Betty initialize variable

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,9 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	for (char c = 'a'; c <= 'z'; c++)
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 	putchar('\n');
 	return (0);
