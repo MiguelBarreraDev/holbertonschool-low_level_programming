146c699731c2d03a416b8942e7dd7db13a2f9010
MiguelBarreraDev <barreram987@gmail.com>
2021-10-02 03:03:07 -0500

    task_1-Modify the characters number of to line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,17 +10,17 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n, lastDigit;
+	int n, lastDig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	lastDigit = n % 10;
-	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	else if (lastDigit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	lastDig = n % 10;
+	if (lastDig > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
+	else if (lastDig == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastDig);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
 	return (0);
 }
