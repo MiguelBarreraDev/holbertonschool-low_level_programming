7eab928c53a58f70ab86816d4801fca055d74a2d
MiguelBarreraDev <barreram987@gmail.com>
2021-10-02 12:22:13 -0500

    print sigle digit number, without variables char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - pritns all single digit numbers of base 10
+ *
+ * Return: Always 0(succes)
+ */
+int main(void)
+{
+	int cont;
+
+	for (cont = 48; cont  <= 57 ; cont++)
+	{
+		putchar(cont);
+	}
+	putchar('\n');
+	return (0);
+}
