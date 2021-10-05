008a01f13496bcc58de48373366e4de3906d5742
MiguelBarreraDev <barreram987@gmail.com>
2021-10-05 10:15:26 -0500

    Program that prints '_putchar' with external library

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <_putchar.c>
+/**
+ * main - print _putchar without standard library
+ *
+ * Return: Always 0(succes)
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
+}
