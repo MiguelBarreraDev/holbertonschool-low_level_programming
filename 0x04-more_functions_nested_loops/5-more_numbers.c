003e2e5b003f14694bf149c89c08206b81aa4d71
MiguelBarreraDev <barreram987@gmail.com>
2021-10-07 16:28:54 -0500

    hat prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int num, cont;
+
+	for (cont = 0; cont < 10; cont++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num < 10)
+				_putchar((num % 10) + '0');
+			else
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+		}
+		_putchar(10);
+	}
+}
