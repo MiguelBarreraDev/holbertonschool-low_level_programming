38573bb5f0e8f12a367c5b26f57a6e9374516d07
MiguelBarreraDev <barreram987@gmail.com>
2021-10-07 13:18:52 -0500

    Add proptoypes-Mandatory-sol-task_0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * _isupper - checks for uppercase character
+ * Return: 1 if is uppercase. 0 for otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return 1;
+	return (0);
+}
