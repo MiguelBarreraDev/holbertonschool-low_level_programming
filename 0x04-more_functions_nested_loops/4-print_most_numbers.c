3cd44e12f14284e4e6b9228d1719b51f03ea2a61
MiguelBarreraDev <barreram987@gmail.com>
2021-10-07 15:29:54 -0500

    Add description of the function print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers from 0 to 9,
+ * print_most_numbers - prints the numbers from 0 to 9,
  * Do not print 2 and 4
  */
 void print_most_numbers(void)
