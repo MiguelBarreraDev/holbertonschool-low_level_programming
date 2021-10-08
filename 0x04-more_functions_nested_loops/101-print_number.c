857b6c73a7808fbede6cd919e89670c95294e3c7
MiguelBarreraDev <barreram987@gmail.com>
2021-10-08 00:23:38 -0500

    Write a function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,30 +7,31 @@
 
 void print_number(int n)
 {
-	int idNegative, digit, order, result, num_0, cont;
+	int idNegative, digit, result;
 
-	order = 0;
+	idNegative = 0;
 	result = 0;
 	if (n < 0)
 	{
 		n *= -1;
 		idNegative = 1;
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
 	while (n > 0)
 	{
 		digit = n % 10;
 		n /= 10;
-		num_0 = 1;
-
-		for (cont = 0; cont < order; cont++)
-		{
-			num_0 *= 10;
-		}
-
-		result += (digit * num_0);
-		order++;
+		result = result * 10 + digit;
 	}
 	if (idNegative == 1)
-		result *= -1;
-	printf("%d\n", result);
+		_putchar('-');
+	while (result > 0)
+	{
+		digit = result % 10;
+		result /= 10;
+		_putchar(digit + '0');
+	}
 }
