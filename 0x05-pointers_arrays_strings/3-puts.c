8102bc56a87b29bf5d79873777f7a09919b7dfb4
MiguelBarreraDev <barreram987@gmail.com>
2021-10-12 03:38:55 -0500

    Function that prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _puts - prints a string in the stdout
+ *
+ * @str: pointer to char
+ */
+void _puts(char *str)
+{
+	int cont = 0;
+	char *save = str;
+
+	while(*(save++))
+		cont++;
+	write(1,str,cont);
+	_putchar(10);
+}
