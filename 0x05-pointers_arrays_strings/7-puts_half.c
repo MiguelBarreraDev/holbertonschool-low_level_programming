35c66d44eaa7bb64e742bc069567873d8d2bb95a
MiguelBarreraDev <barreram987@gmail.com>
2021-10-12 11:39:14 -0500

    Modify the character length of the stdout

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	while (*(save++))
 		cont++;
 	ctn = cont / 2;
-	posHalf = (cont % 2 == 0)? ctn : ctn + 1 ;
+	posHalf = (cont % 2 == 0) ? ctn : ctn + 1;
 	str += posHalf;
 	for (pos = 0; pos < posHalf; pos++)
 	{
