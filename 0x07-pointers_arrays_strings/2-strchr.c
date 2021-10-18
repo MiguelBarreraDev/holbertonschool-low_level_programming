6a296c9d0315e4a77ade6c5c213045d64cafd5a7
MiguelBarreraDev <barreram987@gmail.com>
2021-10-18 14:55:16 -0500

    Add description

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int pos = 0;
 
-	while (s[pos])
+	do
 	{
-		if (s[pos] == c)
-			return (s + pos);
-		pos++;
-	}
-	return (s + pos);
+		if (*s == c)
+			break;
+	} while (*s++);
+	return (s);
 }
