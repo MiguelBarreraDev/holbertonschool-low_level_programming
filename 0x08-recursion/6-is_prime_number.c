3f5dd9967d957ef22fdddef1d281e33d8cdab4de
MiguelBarreraDev <barreram987@gmail.com>
2021-10-20 14:25:11 -0500

    add decription

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,14 +10,16 @@ int is_prime(int, int);
 int is_prime_number(int n)
 {
 	if (n == 0 || n == 1 || n < 0)
-		return 0;
+		return (0);
 	else
-		return (is_prime(n,2));
+		return (is_prime(n, 2));
 }
 /**
  * is_prime - algorithm
- *
+ * @n: integer number
  * @div: divider
+ *
+ * Return: 1 if is prime. 0 other
  */
 int is_prime(int n, int div)
 {
@@ -26,5 +28,5 @@ int is_prime(int n, int div)
 	if (n % div == 0)
 		return (0);
 	else
-		return is_prime(n, div + 1);
+		return (is_prime(n, div + 1));
 }
