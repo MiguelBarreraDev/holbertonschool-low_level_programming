3f42b433e06ed5e86713805576735c64e55c3393
MiguelBarreraDev <barreram987@gmail.com>
2021-10-22 14:02:55 -0500

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
+#include <ctype.h>
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+int validateNumber(char *n);
 /**
  * main - program that adds positive numbers.
  *
@@ -11,7 +13,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int pos, res = 0, num = 0;
+	int pos, res = 0, num = 0, band = 1;
 
 	if (argc == 1)
 		printf("0\n");
@@ -19,16 +21,33 @@ int main(int argc, char *argv[])
 	{
 		for (pos = 1; pos < argc; pos++)
 		{
-			num = atoi(argv[pos]);
-			if (!num)
+			band = validateNumber(argv[pos]);
+
+			if (band == 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
+			num = atoi(argv[pos]);
 			res += num;
 		}
 		printf("%d\n", res);
 	}
 	return (0);
 }
+/**
+ * validateNumber - validate integer number
+ *
+ * @n: pointer a char
+ *
+ * Return: 1 if is a number. 0 if not is a numbe
+ */
+int validateNumber(char *n)
+{
+	if (*n == '\0')
+		return (1);
+	if (!isdigit(*n))
+		return (0);
 
+	return (validateNumber(n + 1));
+}
