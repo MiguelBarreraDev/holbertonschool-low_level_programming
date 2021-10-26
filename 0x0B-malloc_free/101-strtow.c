cbd6db9151331898f9b027f057231ba64be5d56d
MiguelBarreraDev <barreram987@gmail.com>
2021-10-26 17:22:17 -0500

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
-
 char *replace_space(char *, int);
 int number_words(char *);
 /**
@@ -16,11 +15,14 @@ char **strtow(char *str)
 	unsigned int pos = 0, posX = 0, row = 0, n_words = 0, len_word = 0;
 	char **collection, *f_str;
 
-	if (str == 0 || strlen(str) == 0)
+	if (str == 0)
 		return ('\0');
 
 	n_words = number_words(str);
 	f_str = replace_space(str, n_words);
+
+	if (strlen(f_str) == 0)
+		return ('\0');
 
 	collection = (char **)malloc(sizeof(char *) * n_words + 1);
 	if (collection == 0)
@@ -58,7 +60,7 @@ char **strtow(char *str)
 		collection[row][posX] = '\0';
 		posX = 0;
 		pos++;
-	}
+	};
 	return (collection);
 }
 /**
@@ -74,11 +76,19 @@ int number_words(char *str)
 
 	for (pos = 0; pos < strlen(str); pos++)
 	{
-		if (str[pos] == ' ')
-			continue;
-		else if (str[pos - 1] != ' ')
-			continue;
-		n++;
+		if (pos > 0)
+		{
+			if (str[pos] == ' ')
+				continue;
+			else if (pos > 0 && str[pos - 1] != ' ')
+				continue;
+			n++;
+		}
+		else
+		{
+			if (str[pos] != ' ')
+				n++;
+		}
 	}
 	return (n);
 }
@@ -92,7 +102,7 @@ int number_words(char *str)
  */
 char *replace_space(char *str, int n_words)
 {
-	unsigned int pos, size = 0;
+	unsigned int pos, size = 0, posX = 0;
 	char *new_str;
 
 	for (pos = 0; pos < strlen(str); pos++)
@@ -106,11 +116,11 @@ char *replace_space(char *str, int n_words)
 	for (pos = 0; pos < strlen(str); pos++)
 	{
 		if (str[pos] != ' ')
-			*new_str++ = str[pos];
+			*(new_str + posX++) = str[pos];
 		if (str[pos] != ' ' && str[pos + 1] == ' ')
-			*new_str++ = ' ';
+			*(new_str + posX++) = ' ';
+
 	}
-	*(new_str - 1) = '\0';
-	new_str -= size;
+	*(new_str + posX - 1) = '\0';
 	return (new_str);
 }
