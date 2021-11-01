ceb77d275495673e8c2db3a46f4efa04021a0b19
MiguelBarreraDev <barreram987@gmail.com>
2021-11-01 11:36:26 -0500

    README.md

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+#define isNULL(value) ((value == NULL) ? "(nil)" : value)
+/**
+ * print_dog -  function that prints a struct dog
+ *
+ * @d: pointer to struct
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", isNULL(d->name));
+	if (!(d->age))
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", isNULL(d->owner));
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - function that creates a new dog.
+ *
+ * @name: pointer to char
+ * @age: pointer to age
+ * @owner: pointer to char
+ *
+ * Return: pointer to struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *n = malloc(sizeof(struct dog));
+
+	n->name = malloc(strlen(name) + 1);
+	n->owner = malloc(strlen(owner) + 1);
+
+	strcpy(n->name, name);
+	n->age = age;
+	strcpy(n->owner, owner);
+
+	return (n);
+}
