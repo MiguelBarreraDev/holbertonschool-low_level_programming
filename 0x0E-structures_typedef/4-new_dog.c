d32278f14834ec89471660cc1039a7b1eac8b883
MiguelBarreraDev <barreram987@gmail.com>
2021-11-01 12:31:14 -0500

    Modify struct name(typedef)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,13 +16,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (n == NULL)
 		return (NULL);
-	if (name == NULL)
-		name = "";
-	if (owner == NULL)
-		owner = "";
 
 	n->name = malloc(strlen(name) + 1);
+	if (n->name == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
 	n->owner = malloc(strlen(owner) + 1);
+	if (n->owner == NULL)
+	{
+		free(n->name);
+		free(n);
+		return (NULL);
+	}
 
 	strcpy(n->name, name);
 	n->age = age;
