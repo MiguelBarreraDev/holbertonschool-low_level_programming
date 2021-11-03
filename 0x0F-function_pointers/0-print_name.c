a6f0f0d237c9060d8c0c989313e7060ff3b00897
MiguelBarreraDev <barreram987@gmail.com>
2021-11-03 11:15:46 -0500

    Start project - with task_1 complet

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that print name
+ *
+ * @name: pointer to char
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
