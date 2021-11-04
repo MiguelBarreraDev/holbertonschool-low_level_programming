2ae4113d5a32ff4acc802244b629e27f4c9fafeb
MiguelBarreraDev <barreram987@gmail.com>
2021-11-04 14:34:18 -0500

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,50 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
+ * print_all - function that prints anything.
  *
- *
- *
+ * @format: constant pointer to char
  */
+void print_all(const char * const format, ...)
+{
+	int index_for = 0;
+	char letter_for = 0, *save_str;
+	va_list ptr;
 
+	va_start(ptr, format);
+	while (*(format + index_for))
+	{
+		letter_for = *(format + index_for);
+		switch (letter_for)
+		{
+			case 'c':
+				printf("%c", va_arg(ptr, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(ptr, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double));
+				break;
+			case 's':
+				save_str = va_arg(ptr, char *);
+				if (save_str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", save_str);
+				break;
+			default:
+				letter_for = '0';
+				break;
+		}
+		if (*(format + index_for + 1) != '\0' && letter_for != '0')
+		{
+			printf(", ");
+		}
+		index_for++;
+	}
+	putchar(10);
+	va_end(ptr);
+}
