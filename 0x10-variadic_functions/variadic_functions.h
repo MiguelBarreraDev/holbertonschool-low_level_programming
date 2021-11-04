aeb1a9f19b2e14de5e5fa3c52ab29a7c9f4d4543
MiguelBarreraDev <barreram987@gmail.com>
2021-11-04 14:15:34 -0500

    add ; - line 6

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-
-int _putchar(char c)
+int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
