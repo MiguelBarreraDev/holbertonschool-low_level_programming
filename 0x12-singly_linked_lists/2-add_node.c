1eac84a5cb3498e28fdb6bc2a08763b4763a0228
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 14:29:41 -0500

    function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,25 @@
 #include "lists.h"
 /**
+ * add_node - function that adds a new node at
+ * the beginning of a list_t list
  *
+ * @head: pointer that point to  pointer to struct list_t
+ * @str: string for add to the  stru the ct
  *
- *
+ * Return: the address of the new element, or NULL if it failed
  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}
