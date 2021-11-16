ceb50f60ae51b599d686de4119d7618768b62d49
MiguelBarreraDev <barreram987@gmail.com>
2021-11-16 13:30:30 -0500

    remove withespace

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
@@ -21,4 +21,4 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	*head = new;
 
 	return (new);
-}
+}
