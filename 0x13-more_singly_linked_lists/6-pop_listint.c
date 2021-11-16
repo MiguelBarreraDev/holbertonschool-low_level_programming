cf7caa5b41546fdebe5439e1f52710ed47ded7aa
MiguelBarreraDev <barreram987@gmail.com>
2021-11-16 14:03:09 -0500

    New restriction line15 - if the linked list is empty

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *save_node = *head;
-	int save_value = (*head)->n;
+	listint_t *save_node;
+	int save_value;
 
+	if (!head)
+		return (0);
+
+	save_value = (*head)->n;
+	save_node = *head;
 	(*head) = (*head)->next;
-
+	
 	free(save_node);
 
 	return (save_value);
