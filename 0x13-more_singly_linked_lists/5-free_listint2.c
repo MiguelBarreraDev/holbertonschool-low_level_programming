f0d0a148cf3645a3e30590498bff3b8c06d2a4bf
MiguelBarreraDev <barreram987@gmail.com>
2021-11-16 13:45:30 -0500

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,15 @@
 #include "lists.h"
 /**
+ * free_listint2 - function that frees a listint_t list
  *
- *
- *
+ * @head: double pointer pointing to a list structure
  */
+void free_listint2(listint_t **head)
+{
+	if (!(*head))
+		return;
 
+	free_listint2(&((*head)->next));
+	free(*head);
+	*head = NULL;
+}
