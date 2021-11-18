a7d9baec7514dcc794e54b686c965c3f4590dc63
MiguelBarreraDev <barreram987@gmail.com>
2021-11-18 13:11:37 -0500

    Add new librarys

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef unsigned int uint;
+
+int _putchar(char c);
+
 unsigned int binary_to_uint(const char *b); 
 void print_binary(unsigned long int n); 
 int get_bit(unsigned long int n, unsigned int index); 
