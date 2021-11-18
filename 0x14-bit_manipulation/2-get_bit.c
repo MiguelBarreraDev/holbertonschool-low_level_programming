1d185ae66e2f8859ac8c3e80ef07f9a6e062fbcd
MiguelBarreraDev <barreram987@gmail.com>
2021-11-18 16:03:56 -0500

    Add restriction for index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1)
 	if (index == 0)
 		return ((n & 1) ? 1 : 0);
