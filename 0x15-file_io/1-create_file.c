25975f2bbc31dfde7d071b122191bf2b87f38972
MiguelBarreraDev <barreram987@gmail.com>
2021-11-22 17:13:27 -0500

    closed file descriptor

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	return (1);
 }
