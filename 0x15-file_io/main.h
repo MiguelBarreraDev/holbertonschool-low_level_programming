072e1438a8c4613da2f17b85a8210b23af1cb41a
MiguelBarreraDev <barreram987@gmail.com>
2021-11-22 19:30:25 -0500

    Add string.h library

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,12 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define STD_E STDERR_FILENO
+#define STD_O STDOUT_FILENO
+#define STD_I STDON_FILENO
+
+int main(int argc, char **argv);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
