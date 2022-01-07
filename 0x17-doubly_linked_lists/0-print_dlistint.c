c5e8f86d651157c10afe436ff85261a5c6aa23e4
MiguelBarreraDev <barreram987@gmail.com>
2022-01-06 21:31:35 -0500

    Add betty comentarys - return value

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,6 +3,8 @@
  * print_dlistint - Write a function that prints all the
  * elements of a dlistint_t list
  * @h: pointer to double linked list
+ *
+ * Return: The number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
