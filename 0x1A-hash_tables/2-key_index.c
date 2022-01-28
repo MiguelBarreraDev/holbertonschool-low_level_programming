48865609d738c6d0fb9d6b0693dcef20efcef946
MiguelBarreraDev <barreram987@gmail.com>
2022-01-28 18:03:43 -0500

    Create function that gives you theindes of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,6 +10,9 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash = 0, index = 0;
 
+	if (key == 0 || size == 0)
+		return (0);
+
 	hash = hash_djb2(key);
 	index = hash % size;
 
