833ef78d63a7a4e94627b7bc191e5af5b29036c2
MiguelBarreraDev <barreram987@gmail.com>
2022-01-28 02:09:59 -0500

    Initializing pointer to linked list in the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,15 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	unsigned long int index = 0;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(table->array))
 		return (NULL);
+
+	for (index = 0; index < size; index++)
+		table->array[index] = NULL;
 
 	return (table);
 }
