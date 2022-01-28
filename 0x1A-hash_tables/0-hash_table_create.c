9850587d5de37253377c29abb5b6eb4a7ff72626
MiguelBarreraDev <barreram987@gmail.com>
2022-01-28 02:33:46 -0500

    add 'array' variable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 	unsigned long int index = 0;
+	hash_node_t **array = NULL;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (!(table->array))
 	{
 		free(table);
@@ -24,8 +25,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (index = 0; index < size; index++)
-		table->array[index] = NULL;
+		array[index] = NULL;
 
+	table->array = array;
 	table->size = size;
 
 	return (table);
