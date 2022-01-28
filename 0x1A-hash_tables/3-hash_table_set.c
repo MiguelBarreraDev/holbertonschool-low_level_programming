97599f3a848a8adc8e76e8fb84e322f3157c063b
MiguelBarreraDev <barreram987@gmail.com>
2022-01-28 03:39:34 -0500

    Return 0 if the hast table does not exist

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *node = NULL, **head = NULL;
 
+	if (!ht)
+		return (0);
 	/*====Get head  of the linked list====*/
 	if (strcmp(key, "") == 0)
 		return (0);
