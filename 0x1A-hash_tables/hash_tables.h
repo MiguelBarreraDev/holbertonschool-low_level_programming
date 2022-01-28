d4f6610f0b6c33ce4664844b127dc5c4b470d3be
MiguelBarreraDev <barreram987@gmail.com>
2022-01-28 17:23:42 -0500

    add prototype

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -88,7 +88,7 @@ shash_table_t *shash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const shash_table_t *ht, const char *key);
+char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
