79912d4cc29bb6c805e34d35a68c54450be2a334
MiguelBarreraDev <barreram987@gmail.com>
2022-03-17 16:03:05 -0500

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,26 +21,16 @@ def island_perimeter(grid):
         perimeter: int
     """
     perimeter = 0
-    top = 0
-    bottom = 0
-    left = 0
-    right = 0
 
     for i, arr in enumerate(grid):
         for j, elm in enumerate(arr):
             if elm == 1:
-                perimeter += 4
-                if i - 1 == -1 or grid[i-1][j] == 1:
-                    top = 1
-                if j + 1 == len(arr) or grid[i][j+1] == 1:
-                    right = 1
-                if i + 1 == len(grid) or grid[i+1][j] == 1:
-                    bottom = 1
-                if j - 1 == -1 or grid[i][j-1] == 1:
-                    left = 1
-                perimeter -= (top + right + bottom + left)
-                top = 0
-                bottom = 0
-                left = 0
-                right = 0
+                if i - 1 == -1 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if j + 1 == len(arr) or grid[i][j+1] == 0:
+                    perimeter += 1
+                if i + 1 == len(grid) or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j - 1 == -1 or grid[i][j-1] == 0:
+                    perimeter += 1
     return perimeter
