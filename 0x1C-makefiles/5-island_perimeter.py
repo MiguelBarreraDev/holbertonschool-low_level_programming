88ebf09e834d63ddbf96bb98a5d47a2e8522dc39
MiguelBarreraDev <barreram987@gmail.com>
2022-03-17 15:28:40 -0500

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,44 @@
+""" Use this module for get the island perimeter
+
+    Functions
+    ---------
+        insland_perimeter(grid)
+"""
+
+
+def island_perimeter(grid):
+    """
+    Functions Definition
+
+    Parameters
+    ----------
+        grid: list
+            listi of list of integers
+    Return
+    ------
+        perimeter: int
+    """
+    perimeter = 0
+    top = 0
+    bottom = 0
+    left = 0
+    right = 0
+
+    for i, arr in enumerate(grid):
+        for j, elm in enumerate(arr):
+            if elm == 1:
+                perimeter += 4
+                if grid[i-1][j] == 1:
+                    top = 1
+                if grid[i][j+1] == 1:
+                    right = 1
+                if grid[i+1][j] == 1:
+                    bottom = 1
+                if grid[i][j-1] == 1:
+                    left = 1
+                perimeter -= (top + right + bottom + left)
+                top = 0
+                bottom = 0
+                left = 0
+                right = 0
+    return perimeter
