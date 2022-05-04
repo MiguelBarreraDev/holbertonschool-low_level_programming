015097addac2cc3cca46c2284207f3f8a68b6ecf
MiguelBarreraDev <barreram987@gmail.com>
2022-05-03 20:09:50 -0500

    remove space

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -37,7 +37,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return  (i);
+			return (i);
 	}
 
 	return (-1);
